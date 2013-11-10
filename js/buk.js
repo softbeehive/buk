07d35a80915d6787e81aa041cfe44575eb9a25b8
Oleksandr Bystrikov <coder@yakviter.com.ua>
2013-11-11 01:28:51 +0200

    Javascript part: switching slides

--- js/buk.js
+++ js/buk.js
@@ -1,19 +1,54 @@
-/*
-    Реализуйте систему для показа презентаций. Подумайте над тем, как реализовать переключение слайдов, навигацию.
-    Предусмотрите возможность размещения нескольких презентаций на одной странице. Рекомендуется использование jQuery.
+/* 
+   Buk: board-like presentation tool
+   Oleksandr Bystrikov, 2013
 */
+
+function bringSidebar() {
+    $(".buk-menu").toggleClass("buk-menu-expanded");
+}
+
+function closeSidebar() {
+    $(".buk-menu").removeClass("buk-menu-expanded");
+}
 
 jQuery(document).ready(function(){
 
     // Switch slides
-    
+    $(".buk-menu-index a").click(function(e) {
+        e.preventDefault();
+
+        var $target = $($(this).attr('href')),
+            $other = $target.siblings('.active');
+
+        if (!$target.hasClass("active")) {
+            $other.each(function(index, self) {
+                var $this = $(this);
+                $this.removeClass("active");
+            });
+            
+            $target.css({
+                    right: -($target.width())
+                }).addClass('active').animate({
+                    right: 0
+                }, 500);
+        }
+
+        // Close sidebar, resizing slide area to prevent overlapping is planned in future release
+        closeSidebar();
+        
+    });
 
     // Sidebar navigation
     $(".buk-menu-gate").click(function(){
-        $(".buk-menu").toggleClass("buk-menu-expanded");
+        bringSidebar();
     });
 
     // Bottom navigation
+    $(".buk-navi-enter").keyup(function(e) {
+        if(e.keyCode == 13){
+            console.log($(this).val());
+        }
+    });
 
     // Multi slide
 });
